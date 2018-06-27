d61efb66caef43dded0968487c3c441b045bcb50
Sean Altemose <sean@seanaltemose.com>
2018-06-27 16:16:12 +0000

    automatically backed up by learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -3,10 +3,10 @@ class Ship
 
   attr_reader :name, :type, :booty
 
-  def initialize(name, type, booty)
-    @name = name
-    @type = type
-    @booty = booty
+  def initialize(args)
+    @name = args[:name]
+    @type = args[:type]
+    @booty = args[:booty]
     @@ships << self
   end
 
